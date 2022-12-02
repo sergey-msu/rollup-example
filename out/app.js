d93fd8f4974741effbd0aadf36b2768485ccb9a8
Serge Polyanskikh <spmathf@gmail.com>
2022-12-02 18:00:47 +0400

    Update app.js

--- out/app.js
+++ out/app.js
@@ -1,9 +1,11 @@
-import { abc, def } from "./bundle.js";
-import { QRCode } from "./bundle.js";
+import { QRCode, create, toCanvas } from "./bundle.js";
+import { TonhubConnector } from "./bundle.js";
 
-console.log(abc());
-console.log(def());
 
-QRCode.toCanvas(id("qrc-id"), "hello bundle", function (error) {
+console.log(create('4444'));
+console.log(QRCode);
+console.log(TonhubConnector);
+
+toCanvas(getElementById("qrc-id"), "hello bundle", function (error) {
     if (error) console.error(error);
 });
